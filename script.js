bbcec6a701fea2b3dd0b1a9665ffcf8509c5ed7b
Kedma Costa <kedmyknow@gmail.com>
2021-06-02 16:58:09 -0300

    :pencil: add scrip

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const key = document.querySelectorAll(".octave1 > div")
+const keys = document.querySelectorAll(".keyboard > div")
 
-key.forEach()
+function markout(key) {
+  key.classList.remove("selecionada")
+  // console.log("markout");
+}
+
+keys.forEach(function (key) {
+  key.onmousedown = function () {
+    // console.log("mark");
+    key.classList.add("selecionada")
+  }
+
+  key.onmouseup = () => markout(key)
+  key.onmouseleave = () => markout(key)
+})
