56153bf82465995a5f71569425644b633ced50e4
Kedma Costa <kedmyknow@gmail.com>
2021-06-07 16:31:21 -0300

    :pencil: update pages

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const keys = document.querySelectorAll(".keyboard > div")
 
 function markout(key) {
-  key.classList.remove("selecionada")
+  key.classList.remove("selected")
   // console.log("markout");
 }
 
 keys.forEach(function (key) {
   key.onmousedown = function () {
     // console.log("mark");
-    key.classList.add("selecionada")
+    key.classList.add("selected")
   }
 
   key.onmouseup = () => markout(key)
